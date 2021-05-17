27999fdb6446d905f2f687433a4e579a17c2f5e8
furqoncreative <furqoncreative24@gmail.com>
2021-05-17 21:14:24 +0700

    adding layout for Scanner Result

--- app/src/main/java/com/bangkit/gamastik/ui/feature/batik/result/ScannerResultActivity.kt
+++ app/src/main/java/com/bangkit/gamastik/ui/feature/batik/result/ScannerResultActivity.kt
@@ -1,4 +1,4 @@
-package com.bangkit.gamastik.ui.feature.scanner
+package com.bangkit.gamastik.ui.feature.batik.result
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,5 +8,6 @@ class ScannerResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner_result)
+        supportActionBar?.hide()
     }
 }

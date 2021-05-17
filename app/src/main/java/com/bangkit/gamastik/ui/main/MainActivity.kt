54459f18cf0d8a3230e709ecacadb3ccc093d83a
furqoncreative <furqoncreative24@gmail.com>
2021-05-17 20:48:09 +0700

    revise navigation

--- app/src/main/java/com/bangkit/gamastik/ui/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/gamastik/ui/main/MainActivity.kt
@@ -1,30 +1,37 @@
 package com.bangkit.gamastik.ui.main
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bangkit.gamastik.R
+import com.bangkit.gamastik.databinding.ActivityMainBinding
+import com.bangkit.gamastik.ui.feature.batik.scanner.BatikScannerActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_quiz
+                R.id.navigation_home, R.id.navigation_quiz
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        binding.fabScanner.setOnClickListener {
+            startActivity(Intent(this, BatikScannerActivity::class.java))
+        }
     }
 }

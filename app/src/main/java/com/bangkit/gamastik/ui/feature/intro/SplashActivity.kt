6dd1d1d687825213e3e044b4be3cf560003b65a5
furqoncreative <furqoncreative24@gmail.com>
2021-05-27 22:40:55 +0700

    update intro feature

--- app/src/main/java/com/bangkit/gamastik/ui/feature/intro/SplashActivity.kt
+++ app/src/main/java/com/bangkit/gamastik/ui/feature/intro/SplashActivity.kt
@@ -5,17 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.bangkit.gamastik.R
+import com.bangkit.gamastik.ui.base.BaseActivity
+import com.bangkit.gamastik.ui.feature.auth.login.LoginActivity
 import com.bangkit.gamastik.ui.main.MainActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler(mainLooper).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            checklogin()
         }, 3000)
 
     }
+
+    private fun checklogin() {
+        val intent: Intent = if (getToken() != null) {
+            Intent(this, MainActivity::class.java)
+        } else {
+            Intent(this, LoginActivity::class.java)
+        }
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+    }
+
 }

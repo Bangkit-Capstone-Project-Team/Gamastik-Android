a6499d1728ae5dc3c9009a1cee8437341a6ac32c
furqoncreative <furqoncreative24@gmail.com>
2021-05-31 16:46:18 +0700

    add product recommendations feature

--- app/src/main/java/com/bangkit/gamastik/ui/feature/recomendation/ProductRecomendationActivity.kt
+++ app/src/main/java/com/bangkit/gamastik/ui/feature/recomendation/ProductRecomendationActivity.kt
@@ -1,15 +1,29 @@
 package com.bangkit.gamastik.ui.feature.recomendation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangkit.gamastik.R
+import com.bangkit.gamastik.databinding.ActivityProductRecomendationBinding
 import com.bangkit.gamastik.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProductRecomendationActivity : BaseActivity() {
+
+    lateinit var binding: ActivityProductRecomendationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_recomendation)
+        binding = ActivityProductRecomendationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        val keyword = intent.getStringExtra(EXTRA_DATA)
+        val url = "https://www.google.co.id/search?safe=strict&output=search&tbm=shop&psb=1&q=$keyword"
+        binding.webViewSuite.startLoading(url)
+    }
+
+    override fun onBackPressed() {
+        if (!binding.webViewSuite.goBackIfPossible()) super.onBackPressed()
+    }
+
+    companion object {
+        const val EXTRA_DATA = "EXTRA_DATA"
     }
 }

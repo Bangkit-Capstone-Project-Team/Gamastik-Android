4b564ce068482aabbd150b3aa91fd38b0176d01e
furqoncreative <furqoncreative24@gmail.com>
2021-06-01 15:52:11 +0700

    update model batik

--- app/src/main/java/com/bangkit/gamastik/ui/feature/batik/result/ScannerResultActivity.kt
+++ app/src/main/java/com/bangkit/gamastik/ui/feature/batik/result/ScannerResultActivity.kt
@@ -4,10 +4,9 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import com.bangkit.gamastik.databinding.ActivityScannerResultBinding
-import com.bangkit.gamastik.ml.BatikModels
+import com.bangkit.gamastik.ml.BatikModel
 import com.bangkit.gamastik.ui.base.BaseActivity
 import com.bangkit.gamastik.ui.feature.recomendation.ProductRecomendationActivity
 import com.bumptech.glide.Glide
@@ -16,7 +15,6 @@ import org.tensorflow.lite.gpu.CompatibilityList
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.model.Model
 import org.tensorflow.lite.support.model.Model.Options
-import java.io.ByteArrayOutputStream
 import java.io.File
 
 
@@ -45,10 +43,13 @@ class ScannerResultActivity : BaseActivity() {
             )
             startActivity(intent)
         }
+        binding.ivBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 
     private fun batikRecognition(img: Bitmap) {
-        val model: BatikModels by lazy {
+        val model: BatikModel by lazy {
 
             val compatList = CompatibilityList()
 
@@ -60,18 +61,24 @@ class ScannerResultActivity : BaseActivity() {
                 Options.Builder().setNumThreads(4).build()
             }
 
-            BatikModels.newInstance(applicationContext, options)
+            BatikModel.newInstance(applicationContext, options)
         }
 
         val image = TensorImage.fromBitmap(img)
         val result = model.process(image).probabilityAsCategoryList.toList()
             .sortedByDescending { it.score }[0]
+
         Glide.with(applicationContext)
             .load(img)
             .into(binding.ivBatikResult)
 
         binding.tvBatikResult.text = result.label
         model.close()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 
 }
